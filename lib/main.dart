22a195b5926f58cdac688611d04d127e15300d9e
Nguyen Phuc Son Tung <52483128+ngphsotu@users.noreply.github.com>
2022-07-26 08:58:31 +0700

    Video Player App UI

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:get/get.dart';
+import 'package:video_player_app/video_info.dart';
+
+import 'home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
